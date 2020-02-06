0db615eb63b37e70a597ed289c0531b75a2bcf00
QA-Doce <thauandoce@gmail.com>
2020-02-06 18:56:39 -0300

    Minha rola na sua mão

---
+++ InForma/Test/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Utilities
+{
+	public static void OpenChrome()
+	{
+		IWebDriver driver = new ChromeDriver(@"C:\Users\Thauan Moser Doce\Downloads"); //<-Add your path
+		driver.Navigate().GoToUrl("https://www.uol.com.br");
+	}
+}
