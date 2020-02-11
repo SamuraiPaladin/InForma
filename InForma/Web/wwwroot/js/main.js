15b4407009161e8877104c6ee4b06dcc57b459c1
beyond-kernel <rafanog.linux@gmail.com>
2020-02-10 23:41:50 -0300

    Commita navegação menu Funcao, Modalidade, Unidade

--- InForma/Web/wwwroot/js/main.js
+++ InForma/Web/wwwroot/js/main.js
@@ -18,5 +18,5 @@ $(document).ready(function () {
 
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
+    var instances = M.Modal.init(elems, {});
 });
