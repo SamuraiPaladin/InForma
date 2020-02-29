be5617d765e893f98f6c03e4023d1d5cfbe56545
QA-Doce <thauandoce@gmail.com>
2020-02-29 12:31:27 -0300

    teste

--- InForma/Test/Selenium/Utilities.cs
+++ InForma/Test/Selenium/Utilities.cs
@@ -1,15 +1,35 @@
 ﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 using System;
+using System.Drawing;
+using System.IO;
+using Test;
+using Xceed.Wpf.Toolkit;
+using Xunit;
 
 /// <summary>
 /// Summary description for Class1
 /// </summary>
 public class Utilities
 {
+	public static IWebDriver driver;
 	public static void OpenChrome()
 	{
-		IWebDriver driver = new ChromeDriver(@"C:\Users\Thauan Moser Doce\Downloads"); //<-Add your path
-		driver.Navigate().GoToUrl("https://www.uol.com.br");
+		var t = Directory.GetCurrentDirectory().Remove(Directory.GetCurrentDirectory().ToString().Length - 26);
+
+		driver = new OpenQA.Selenium.Chrome.ChromeDriver(t); //<-Add your path
+		driver.Manage().Window.Maximize();
+		////driver.Manage().Window.Size = new Size(1366, 800);
+		driver.Navigate().GoToUrl("http://localhost:5200/");
+
 	}
+
+	public static void CepValidates(String Endereco, String Bairro, String Cidade, String Estado)
+	{
+		PageObject.RegisterUnitScreen.EnderecoTextBox(Endereco);
+		PageObject.RegisterUnitScreen.BairroTextBox(Bairro);
+		PageObject.RegisterUnitScreen.CidadeTextBox(Cidade);
+		PageObject.RegisterUnitScreen.EstadoTextBox(Estado);
+	}
+
+
 }
